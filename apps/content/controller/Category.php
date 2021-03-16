a73c8f1d35d190e289c9fe40793c201987219117
garfunkel <garfunkel@126.com>
2021-03-16 18:02:13 +0800

    content scoll

--- apps/content/controller/Category.php
+++ apps/content/controller/Category.php
@@ -100,9 +100,9 @@ class Category extends Adminbase
     {
         if (Request::instance()->isPost()) {
             $Category = Loader::model("content/Category");
-
-            $catid = $Category->addCategory(Request::instance()->post());
             var_dump(Request::instance()->post());die();
+            $catid = $Category->addCategory(Request::instance()->post());
+
             if ($catid) {
                 $this->success("添加成功！", Url::build("Category/index"));
             } else {
